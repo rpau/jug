51fc8219dc5d5e521daa96bbed672a6548634f07
coroleu <acoroleu@gmail.com>
2014-02-26 21:50:08 +0100

    Initial commit

---
+++ src/main/java/org/walkmod/jug/entities/Tweet.java
@@ -0,0 +1,11 @@
+package org.walkmod.jug.entities;
+
+public class Tweet {
+	
+	private User user;
+	
+	public Tweet(User user){
+		this.user = user;
+	}
+
+}
---
+++ src/main/java/org/walkmod/jug/entities/User.java
@@ -0,0 +1,5 @@
+package org.walkmod.jug.entities;
+
+public class User {
+
+}
