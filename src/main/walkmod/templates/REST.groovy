0ae05ff7c073c4c1efe29d7dccf53adaa5f22aa4
rpau <raquelpau@gmail.com>
2014-03-02 19:58:10 +0100

    last changes

--- src/main/walkmod/templates/REST.groovy
+++ src/main/walkmod/templates/REST.groovy
@@ -7,5 +7,23 @@ import javax.ws.rs.core.Response;
 
 @Path("/twitter")
 public class Twitter{
+
+	@GET
+	@Path("/findAll${query.resolve("type.name")}s/{page}/{rows}")
+	public Response findAll${query.resolve("type.name")}s(@PathParam("page") int page, @PathParam("rows") int rows) {
+		String str = "";
+
+		try {
+			List ${query.resolve("type.name").toLowerCase()}s = new LinkedList();
+			${query.resolve("type.name").toLowerCase()}s = TwitterFacade.getInstance().findAll${query.resolve("type.name")}s(page, rows);
+			ObjectMapper mapper = new ObjectMapper();
+			str = mapper.writeValueAsString(${query.resolve("type.name").toLowerCase()}s);
+
+		} catch (Exception e) {
+			Response.serverError().build();
+		}
+		return Response.status(200).entity(str).build();
+
+	}
 	
 }
