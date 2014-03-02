725dd856df48165d956364f6e30c4acd86000707
rpau <raquelpau@gmail.com>
2014-03-02 18:00:06 +0100

    walkmod part

---
+++ src/main/walkmod/templates/hibernate.groovy
@@ -0,0 +1,20 @@
+package ${query.resolve("root.package.name")};
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@Entity
+@Table(name = "${query.resolve("type.name")}")
+public class ${query.resolve("type.name")} implements Serializable {
+
+<% query.resolve("type.fields").each{ 
+
+	field ->
+	out << "@Column( name=\"${field.variables.get(0).id}\") ${field}";
+	
+ } 
+ %>
+
+}
