16076d314351c2c2ec053527019792b70a8e58c4
rpau <raquelpau@gmail.com>
2014-04-13 16:24:12 +0200

    update

--- src/main/java/org/walkmod/jug/model/Tweet.java
+++ src/main/java/org/walkmod/jug/model/Tweet.java
@@ -1,47 +1,15 @@
-/* 
-  Copyright (C) 2013 Raquel Pau and Albert Coroleu.
- 
- Walkmod is free software: you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- Walkmod is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Lesser General Public License for more details.
- 
- You should have received a copy of the GNU Lesser General Public License
- along with Walkmod.  If not, see <http://www.gnu.org/licenses/>.*/
 package org.walkmod.jug.model;
 
 import java.util.Date;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Column;
+import java.util.Calendar;
 
-@SuppressWarnings("serial")
-@Entity
-@Table(name = "Tweet")
-public class Tweet implements Serializable {
+public class Tweet {
 
-	@Column(name = "user")
 	private User user;
 
-	@Column(name = "text")
-	private String text;
+	public String text;
 
-	@Column(name = "date")
 	private Date date;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	@Column(name = "id")
-	private Integer id;
 
 	public Tweet() {
 	}
@@ -52,48 +20,5 @@ public class Tweet implements Serializable {
 		date = new Date();
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (o instanceof Tweet) {
-			return id.equals(((Tweet) o).id);
-		}
-		return false;
-	}
 
-	@Override
-	public int hashCode() {
-		return id.hashCode();
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Integer getId() {
-		return id;
-	}
 }
